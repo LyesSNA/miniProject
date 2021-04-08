78baea4a7ff385e6542a73fc7280b90b7a02d630
Lyes <lyes-sna@lyes>
2021-04-09 00:01:13 +0200

    not very efficient

--- src/org/paumard/elevator/student/EqualityElevator.java
+++ src/org/paumard/elevator/student/EqualityElevator.java
@@ -8,89 +8,107 @@ import org.paumard.elevator.model.Person;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EqualityElevator implements Elevator {
+    private static final int ANGER_LIMIT_THRESHOLD = 180;
 	private DIRECTION direction = DIRECTION.UP;
-	private int currentFloor = 1;
+    private int currentFloor = 1;
 	private List<List<Person>> peopleByFloor = List.of();
-	private int capacity;
-	private LocalTime time;
 	private List<Person> people = new ArrayList<>();
+	private final int capacity;
+	private LocalTime time;
 	private List<Integer> destinations = new ArrayList<>();
 
-	public EqualityElevator(int capacity) {
+    public EqualityElevator(int capacity) {
 		this.capacity = capacity;
-	}
+    }
 
-	@Override
-	public void startsAtFloor(LocalTime time, int initialFloor) {
-		LocalTime arrivalTime = LocalTime.of(5,0,0);
-		Duration.between(arrivalTime, time);
-
+    @Override
+    public void startsAtFloor(LocalTime time, int initialFloor) {
 		this.time = time;
-	}
+    }
 
-	@Override
-	public void peopleWaiting(List<List<Person>> peopleByFloor) {
-		this.peopleByFloor = peopleByFloor;
-	}
+    @Override
+    public void peopleWaiting(List<List<Person>> peopleByFloor) {
+    	this.peopleByFloor = peopleByFloor;
+    }
 
-	@Override
-	public List<Integer> chooseNextFloors() {
+    @Override
+    public List<Integer> chooseNextFloors() {
+    	
+    	if (!this.destinations.isEmpty()) {
+    		return this.destinations;
+    	}
+        
+    	int numberOfPeopleWaiting = countWaitingPeople();
+    	if (numberOfPeopleWaiting > 0) {
+    		
+    		List<Integer> destinations = destinationsToPickUpAngryPeople();
+    		if (!destinations.isEmpty()) {
+    			this.destinations = destinations;
+    			return this.destinations;
+    		}
+    		
+    		List<Integer> nonEmptyFloors = findNonEmptyFloor();
+    		int nonEmptyFloor = nonEmptyFloors.get(0);
+    		if (nonEmptyFloor != this.currentFloor) {
+    			return List.of(nonEmptyFloor);
+    		} else {
+    			int indexOfCurrentFloor = this.currentFloor - 1;
+				List<Person> waitingListForCurrentFloor = 
+						this.peopleByFloor.get(indexOfCurrentFloor);
+				
+				List<Integer> destinationFloorsForCurrentFloor = 
+						findDestinationFloors(waitingListForCurrentFloor);
+				this.destinations  = destinationFloorsForCurrentFloor;
+				return this.destinations;
+    		}
+    	}
+    	
+    	return List.of(1);
+    }
 
-		if(!this.destinations.isEmpty()) {
-			return this.destinations;
-		}
+	private List<Integer> destinationsToPickUpAngryPeople() {
 		
-		
-		int nulbreOfPeopleWaiting = countWaitingPeople();
-		if(nulbreOfPeopleWaiting > 0) {
-
-
-			List<Integer> findNonEmptyFloors = findNonEmptyFloor();
-			int nonEmptyFloor = findNonEmptyFloors.get(0);
-			if( nonEmptyFloor != currentFloor)
-				return findNonEmptyFloors;
-			else {
-				int indexOfCurrentFloor = this.currentFloor -1;
-				List<Person> wairingListForCurretnFloor = 
-						this.peopleByFloor.get(indexOfCurrentFloor );
-				
-				List <Integer> destinationFloorsForCurrentFloor = 
-						findDestinationFloors(wairingListForCurretnFloor);
-				
-				this.destinations = destinationFloorsForCurrentFloor;
-				
-				return destinationFloorsForCurrentFloor;
+		for (int indexFloor = 0 ; indexFloor < Building.MAX_FLOOR ; indexFloor++) {
+			List<Person> waitingList = this.peopleByFloor.get(indexFloor);
+			if (!waitingList.isEmpty()) {
+				Person mostPatientPerson = waitingList.get(0);
+				LocalTime arrivalTime = mostPatientPerson.getArrivalTime();
+				Duration waitingTime = Duration.between(arrivalTime, this.time); 
+				long waitingTimeInSeconds = waitingTime.toSeconds();
+				if (waitingTimeInSeconds >= ANGER_LIMIT_THRESHOLD) {
+					List<Integer> result = List.of(indexFloor + 1, mostPatientPerson.getDestinationFloor());
+					return new ArrayList<>(result);
+				}
 			}
 		}
-		return List.of(1);
+		return List.of();
 	}
 
-	private List<Integer> findDestinationFloors(List<Person> wairingListForCurretnFloor) {
-		return wairingListForCurretnFloor.stream()
-									.map(person -> person.getDestinationFloor())
-									.distinct()
-									.sorted()
-									.collect(Collectors.toList());
+	private List<Integer> findDestinationFloors(List<Person> waitingListForCurrentFloor) {
+		return waitingListForCurrentFloor.stream()
+			.map(person -> person.getDestinationFloor())
+			.distinct()
+			.sorted()
+			.collect(Collectors.toList());
 	}
 
 	private List<Integer> findNonEmptyFloor() {
-		for(int indexFloor = 0; indexFloor < Building.MAX_FLOOR; indexFloor ++) {
+		for (int indexFloor = 0 ; indexFloor < Building.MAX_FLOOR ; indexFloor++) {
 			if (!peopleByFloor.get(indexFloor).isEmpty()) {
 				return List.of(indexFloor + 1);
 			}
 		}
-		return List.of(1);
+		return List.of(-1);
 	}
 
 	private int countWaitingPeople() {
 		return peopleByFloor.stream()
-				.mapToInt(list -> list.size())
-				.sum();
+			.mapToInt(list -> list.size())
+			.sum();
 	}
 
 	@Override
@@ -103,9 +121,7 @@ public class EqualityElevator implements Elevator {
 		if(!peopleByFloor.get(indexOfCurrentFloor).isEmpty()) {
 			List<Integer> intermediateDestinations = findDestinationFloors(peopleByFloor.get(floor -1));	
 			List<Integer> additionnaFloors = weCanTakeThem(intermediateDestinations);
-			System.out.println( "\n\n\t\t\t" + destinations + "\t\t\t\t" + additionnaFloors +"\n");
 			destinations.addAll(destinations.size(), additionnaFloors);
-			System.out.println("\t\t\t\t\n" + destinations);
 		}
 		
 		this.currentFloor = floor;
@@ -116,7 +132,7 @@ public class EqualityElevator implements Elevator {
 		this.people.addAll(people);  
 		int indexFloor = this.currentFloor -1;
 		this.peopleByFloor.get(indexFloor).removeAll(people);
-
+		
 	}
 
 	@Override
@@ -131,10 +147,10 @@ public class EqualityElevator implements Elevator {
 		signFromDirection = signFromEnum();
 		
 		List<Integer> additionnalFloors = intermediateDestinations.stream()
-								.filter(d -> signFromDirection * d > signFromDirection * (indexOfCurrentFloor) && !destinations.contains(d))
-								.distinct()
-								.sorted()
-								.collect(Collectors.toList());
+				.filter(d -> signFromDirection * d > signFromDirection * (indexOfCurrentFloor) && !destinations.contains(d))
+				.distinct()
+				.sorted()
+				.collect(Collectors.toList());
 		return additionnalFloors;
 	}
 
@@ -146,23 +162,24 @@ public class EqualityElevator implements Elevator {
 			signFromDirection = -1;
 		return signFromDirection;
 	}
+   
 
-	@Override
-	public void newPersonWaitingAtFloor(int floor, Person person) {
-		int indexFloor = floor -1;
-		this.peopleByFloor.get(indexFloor).add(person);
-	}
+    @Override
+    public void newPersonWaitingAtFloor(int floor, Person person) {
+    	int indexFloor = floor - 1;
+    	this.peopleByFloor.get(indexFloor).add(person);
+    }
 
-	@Override
-	public void lastPersonArrived() {
-	}
+    @Override
+    public void lastPersonArrived() {
+    }
 
-	@Override
-	public void timeIs(LocalTime time) {
-		this.time = time;
-	}
+    @Override
+    public void timeIs(LocalTime time) {
+    	this.time = time;
+    }
 
-	@Override
-	public void standByAtFloor(int currentFloor) {
-	}
+    @Override
+    public void standByAtFloor(int currentFloor) {
+    }
 }
